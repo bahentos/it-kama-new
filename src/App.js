0ff7ffc5b613c11a46629051e2a547266d7a516d
Alexander <bahentos@gmail.com>
2022-05-10 18:31:42 +0300

    18. Уроки React JS (страница Dialogs)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Dialogs from "./compontents/Dialogs/Dialogs";
 import Header from "./compontents/Header/Header";
 import Navbar from "./compontents/Navbar/Navbar";
 import Profile from './compontents/Profile/Profile';
@@ -6,9 +7,16 @@ import Profile from './compontents/Profile/Profile';
 const App = () => {
   return (
     <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
+      <div className="app-wrapper-header">
+        <Header />
+      </div>
+      <div className="app-wrapper-navbar">
+        <Navbar />
+      </div>
+      <div className="app-wrapper-content">
+        {/* <Profile /> */}
+        <Dialogs />
+      </div>
     </div>
   );
 };
---
+++ src/compontents/Dialogs/Dialogs.jsx
@@ -0,0 +1,11 @@
+import s from "./Dialogs.module.css";
+
+const Dialogs = (props) => {
+  return (
+    <>
+      <div className={s.container}>Dialogs</div>
+    </>
+  );
+};
+
+export default Dialogs;
