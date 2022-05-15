09d54193a32c8a48b5482d8d1cccd52f23190f55
Alexander <bahentos@gmail.com>
2022-05-15 21:29:57 +0300

    24. Уроки React JS (UI - BLL, выносим данные в сторону)

--- src/compontents/Dialogs/Dialogs.jsx
+++ src/compontents/Dialogs/Dialogs.jsx
@@ -3,6 +3,7 @@ import s from "./Dialogs.module.css";
 
 const DialogItem = (props) => {
   let path = '/dialogs/' + props.id
+  
   return (
     <NavLink className={s.dialog}
       style={({ isActive }) => {
@@ -26,22 +27,36 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+  let dialogsData = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Anny' },
+    { id: 3, name: 'Stieven' },
+    { id: 4, name: 'Tony' },
+    { id: 5, name: 'Larry' }
+  ]
+  let messagesData = [
+    { id: 1, message: 'Do esse exercitation aliqua ea ullamco est anim do nostrud magna tempor.' },
+    { id: 2, message: 'Nulla ea eiusmod magna anim ex commodo ullamco duis nulla anim cupidatat do eiusmod ut.' },
+    { id: 3, message: 'Ea ea nisi voluptate deserunt Lorem proident.' },
+    { id: 4, message: 'Lorem fugiat exercitation in magna et laboris cillum amet enim excepteur irure ullamco.' },
+    { id: 5, message: 'Labore Lorem et veniam est.' }
+  ]
   return (
     <>
       <div className={s.container}>
         <div className={s.dialogsItems}>
-          <DialogItem id='1' name='John'></DialogItem>
-          <DialogItem id='2' name='Anny'></DialogItem>
-          <DialogItem id='3' name='Stieven'></DialogItem>
-          <DialogItem id='4' name='Tony'></DialogItem>
-          <DialogItem id='5' name='Larry'></DialogItem>
+          <DialogItem id={dialogsData[0].id} name={dialogsData[0].name}></DialogItem>
+          <DialogItem id={dialogsData[1].id} name={dialogsData[1].name}></DialogItem>
+          <DialogItem id={dialogsData[2].id} name={dialogsData[2].name}></DialogItem>
+          <DialogItem id={dialogsData[3].id} name={dialogsData[3].name}></DialogItem>
+          <DialogItem id={dialogsData[4].id} name={dialogsData[4].name}></DialogItem>
         </div>
         <div className={s.messagesItems}>
-          <Message text='Do esse exercitation aliqua ea ullamco est anim do nostrud magna tempor.'></Message>
-          <Message text='Nulla ea eiusmod magna anim ex commodo ullamco duis nulla anim cupidatat do eiusmod ut.'></Message>
-          <Message text='Ea ea nisi voluptate deserunt Lorem proident.'></Message>
-          <Message text='Lorem fugiat exercitation in magna et laboris cillum amet enim excepteur irure ullamco.'></Message>
-          <Message text='Labore Lorem et veniam est.'></Message>
+          <Message text={messagesData[0].message}></Message>
+          <Message text={messagesData[1].message}></Message>
+          <Message text={messagesData[2].message}></Message>
+          <Message text={messagesData[3].message}></Message>
+          <Message text={messagesData[4].message}></Message>
         </div>
       </div>
     </>
--- src/compontents/Profile/MyPosts/MyPosts.jsx
+++ src/compontents/Profile/MyPosts/MyPosts.jsx
@@ -4,6 +4,12 @@ import Post from "./Post/Post";
 //Какой-то комментарий
 
 const MyPosts = () => {
+  let postsData = [
+    { id: 1, post: 'Est et cupidatat minim voluptate eiusmod dolore commodo culpa proident pariatur consectetur.' , like_count: 2},
+    { id: 2, post: 'Exercitation et anim minim voluptate ad nisi commodo ipsum commodo ad et elit laboris.' , like_count: 5},
+    { id: 3, post: 'Ut velit pariatur ex ex ipsum veniam laboris ipsum tempor ipsum enim velit.' , like_count: 7},
+    { id: 4, post: 'Et excepteur do est labore velit deserunt duis.' , like_count: 22},
+  ]
   return (
     <div className={s.container}>
       My posts
@@ -14,10 +20,10 @@ const MyPosts = () => {
         <button className={s.btn}>Add post</button>
       </div >
       <div className={s.messageContainer}>
-        <Post like_count='2' message='Est et cupidatat minim voluptate eiusmod dolore commodo culpa proident pariatur consectetur.' />
-        <Post like_count='5' message='Exercitation et anim minim voluptate ad nisi commodo ipsum commodo ad et elit laboris.' />
-        <Post like_count='7' message='Ut velit pariatur ex ex ipsum veniam laboris ipsum tempor ipsum enim velit.' />
-        <Post like_count='22' message='Et excepteur do est labore velit deserunt duis.' />
+        <Post like_count={postsData[0].like_count} message={postsData[0].post} />
+        <Post like_count={postsData[1].like_count} message={postsData[1].post} />
+        <Post like_count={postsData[2].like_count} message={postsData[2].post} />
+        <Post like_count={postsData[3].like_count} message={postsData[3].post} />
       </div>
     </div>
   );
