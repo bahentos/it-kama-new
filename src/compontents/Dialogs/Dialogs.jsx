4324c02eb2f214bbbbc4bcf33bd2ae24aab8b946
Alexander <bahentos@gmail.com>
2022-05-20 00:24:08 +0300

    re-design dialogs

--- src/compontents/Dialogs/Dialogs.jsx
+++ src/compontents/Dialogs/Dialogs.jsx
@@ -6,7 +6,6 @@ import Message from "./Message/Message";
 const Dialogs = (props) => {
 
   let dialogsElement = props.dialogsPage.dialogsData.map( dialog =>  <DialogItem id={dialog.id} name={dialog.name}></DialogItem>)
-  let messagesElement = props.dialogsPage.messagesData.map( message_item =>  <Message text={message_item.message}></Message>)
   
   return (
     <>
@@ -15,7 +14,7 @@ const Dialogs = (props) => {
           {dialogsElement}
         </div>
         <div className={s.messagesItems}>
-          {messagesElement}
+          <Message messagesData={props.dialogsPage.messagesData} />
         </div>
       </div>
     </>
--- src/compontents/Dialogs/Message/Message.jsx
+++ src/compontents/Dialogs/Message/Message.jsx
@@ -1,10 +1,17 @@
 import s from "./Message.module.css";
+import triangle from './img/triangle.svg'
 
 
 const Message = (props) => {
+  let messages = props.messagesData.map( message => 
+    (<div className={s.messageContainer}>
+      <img src={triangle} alt="" />
+      <div className={s.message}>{message.message}</div>
+    </div>)
+   )
   return (
-    <div className={s.messages}>
-      {props.text}
+    <div className={s.messagesArea}>
+      {messages}
     </div>
   )
 }
