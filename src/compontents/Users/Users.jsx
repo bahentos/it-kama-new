30e2024aecd2c00538654e1bdb69342c991d56fc
Alexander <bahentos@gmail.com>
2022-05-31 00:38:31 +0300

    53 - React JS практика - классовый компонент (extends React.Component)

--- src/compontents/Users/Users.jsx
+++ src/compontents/Users/Users.jsx
@@ -1,48 +1,49 @@
-import * as axios from 'axios'
+import React from "react";
+import axios from "axios";
 import s from './Users.module.css'
 import icon_user from './img/userDefaults.svg'
 
-const Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                props.set_users(response.data.items)
+
+
+class Users extends React.Component {
+    constructor (props) {
+        super(props)
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                this.props.set_users(response.data.items)
             })
-        }
     }
     
-    let users_divs = props.users.map((user, index) => {
-        let follow = (userId) => {
-            props.isFollow(userId)
-        }
-        return (
-            <div className={s.user} key={index + '_user'}>
-                <div className={s.imgContainer}>
-                    <div className={s.img}>
-                        <img src={user.photos.small != null? user.photos.small : icon_user} alt="" />
+    render() {
+        let users_divs = this.props.users.map((user, index) => {
+            return (
+                <div className={s.user} key={index + '_user'}>
+                    <div className={s.imgContainer}>
+                        <div className={s.img}>
+                            <img src={user.photos.small != null? user.photos.small : icon_user} alt="" />
+                        </div>
+                        <button onClick={() => {this.props.isFollow(user.id) }} className={s.follow}>{user.followed ? 'Follow' : 'Unfollow'}</button>
                     </div>
-                    <button onClick={() => { props.isFollow(user.id) }} className={s.follow}>{user.followed ? 'Follow' : 'Unfollow'}</button>
+                    <div className={s.nameContainer}>
+                        <div className={s.name}>{user.name}</div>
+                        <div className={s.status}>{user.status}</div>
+                    </div>
+                    <div className={s.locationContainer}>
+                        <div className={s.country}>{'user.location.country'}</div>
+                        <div className={s.city}>{'user.location.city'}</div>
+                    </div>
                 </div>
-                <div className={s.nameContainer}>
-                    <div className={s.name}>{user.name}</div>
-                    <div className={s.status}>{user.status}</div>
-                </div>
-                <div className={s.locationContainer}>
-                    <div className={s.country}>{'user.location.country'}</div>
-                    <div className={s.city}>{'user.location.city'}</div>
+            )
+        })
+    
+        return (
+            <div className={s.fon}>
+                <div className={s.container}>
+                    {users_divs}
                 </div>
             </div>
         )
-    })
-
-    return (
-        <div className={s.fon}>
-                     <button onClick={getUsers}>Get Users</button>
-            <div className={s.container}>
-                {users_divs}
-            </div>
-        </div>
-    )
+    }
 }
 
 export default Users
